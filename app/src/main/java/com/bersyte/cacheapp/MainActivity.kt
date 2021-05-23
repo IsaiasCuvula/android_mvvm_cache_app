a92842ab07b66d2a75374067ecbb452967f75383
Isaias Cuvula <bersyteinfo@gmail.com>
2021-05-23 17:03:29 +0300

    recyclerView added

--- app/src/main/java/com/bersyte/cacheapp/MainActivity.kt
+++ app/src/main/java/com/bersyte/cacheapp/MainActivity.kt
@@ -1,16 +1,75 @@
 package com.bersyte.cacheapp
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bersyte.cacheapp.adapter.CacheAppAdapter
 import com.bersyte.cacheapp.databinding.ActivityMainBinding
+import com.bersyte.cacheapp.viewmodel.CacheAppViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: CacheAppViewModel by viewModels()
+    private lateinit var cacheAppAdapter: CacheAppAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        populateUI()
     }
+
+
+    private fun populateUI() {
+        cacheAppAdapter = CacheAppAdapter()
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            setHasFixedSize(true)
+            adapter = cacheAppAdapter
+        }
+
+
+        viewModel.countriesResponse.observe(this, { countries ->
+            cacheAppAdapter.countries = countries
+        })
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
