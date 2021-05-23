946d885f5f88bdfc047b138d30e9512d442e1f2c
Isaias Cuvula <bersyteinfo@gmail.com>
2021-05-23 16:58:28 +0300

    adapter added

--- app/src/main/java/com/bersyte/cacheapp/adapter/CacheAppAdapter.kt
+++ app/src/main/java/com/bersyte/cacheapp/adapter/CacheAppAdapter.kt
@@ -1,12 +1,59 @@
 package com.bersyte.cacheapp.adapter
 
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.*
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import coil.load
+import com.bersyte.cacheapp.databinding.LayoutAdapterBinding
+import com.bersyte.cacheapp.models.CountriesItem
 
-class CacheAppAdapter: Adapter<CacheAppAdapter.CacheAppViewHolder>() {
+class CacheAppAdapter : Adapter<CacheAppAdapter.CacheAppViewHolder>() {
 
 
-    inner class CacheAppViewHolder():
+    inner class CacheAppViewHolder(val binding: LayoutAdapterBinding) :
         ViewHolder(binding.root)
+
+    private val diffCallback = object : DiffUtil.ItemCallback<CountriesItem>() {
+        override fun areItemsTheSame(oldItem: CountriesItem, newItem: CountriesItem): Boolean {
+            return oldItem.name == newItem.name
+        }
+
+        override fun areContentsTheSame(oldItem: CountriesItem, newItem: CountriesItem): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, diffCallback)
+    var countries: List<CountriesItem>
+        get() = differ.currentList
+        set(value) {
+            differ.submitList(value)
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CacheAppViewHolder {
+        return CacheAppViewHolder(
+            LayoutAdapterBinding.inflate(
+                LayoutInflater.from(parent.context), parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: CacheAppViewHolder, position: Int) {
+        val country = countries[position]
+
+        holder.binding.apply {
+            ivFlag.load(country.flag)
+            tvName.text = country.name
+            tvCapital.text = country.capital
+            tvPopulation.text = country.population.toString()
+            tvNumericCode.text = country.callingCodes.toString()
+        }
+    }
+
+    override fun getItemCount() = countries.size
 
 }
--- app/src/main/java/com/bersyte/cacheapp/models/CountriesItem.kt
+++ app/src/main/java/com/bersyte/cacheapp/models/CountriesItem.kt
@@ -2,16 +2,9 @@ package com.bersyte.cacheapp.models
 
 
 data class CountriesItem(
-    val altSpellings: List<String>,
-    val area: Double,
     val callingCodes: List<String>,
     val capital: String,
-    val cioc: String,
-    val demonym: String,
     val flag: String,
-    val latlng: List<Double>,
     val name: String,
-    val nativeName: String,
-    val numericCode: String,
     val population: Int
 )
